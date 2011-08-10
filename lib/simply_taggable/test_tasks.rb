e86e47718014d3477e34d2227046ed1b99a01961
Jake <github@jakewendt.com>
2011-08-10 11:12:04 -0700

    Better inclusion of gem dirs to test.

--- lib/simply_taggable/test_tasks.rb
+++ lib/simply_taggable/test_tasks.rb
@@ -31,8 +31,18 @@ Rake::Task['test:units'].prerequisites.unshift(
 #		tests in the context of the application
 #
 @gem_test_dirs ||= []
-@gem_test_dirs << File.expand_path(File.join(File.dirname(__FILE__),
-	'/../../test/unit/taggable/'))
-@gem_test_dirs << File.expand_path(File.join(File.dirname(__FILE__),
-	'/../../test/functional/taggable/'))
+#@gem_test_dirs << File.expand_path(File.join(File.dirname(__FILE__),
+#	'/../../test/unit/taggable/'))
+#@gem_test_dirs << File.expand_path(File.join(File.dirname(__FILE__),
+#	'/../../test/functional/taggable/'))
 
+#
+#	More flexible. Find all test files, pick out their dir, uniq 'em and add.
+#
+Dir.glob( File.expand_path(File.join(File.dirname(__FILE__),
+	'/../../test/*/taggable/*_test.rb'))
+).collect{|f|
+	File.dirname(f)
+}.uniq.each{ |dir|
+	@gem_test_dirs << dir
+}
